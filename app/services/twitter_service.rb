65b9814a3c01afbd23d485a92b7a7af49a13eb75
Roberto Cunha <roberto@MacBook-Pro-de-usuario.local>
2021-09-01 23:56:27 -0300

    Melhora no service de comunicação com a api do Twitter

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,5 +1,5 @@
 class TwitterService
-  def self.get_data(url)
+  def self.get_video(url)
     id = url.split('/')[-1]
     final_url = "https://api.twitter.com/1.1/statuses/show.json?id=#{id}&include_entities=true&tweet_mode=extended"
     html = HTTParty.get(
@@ -8,7 +8,10 @@ class TwitterService
         'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAMwHTQEAAAAA0VLKmz5mDc67O2Z6gUB60R1rCV0%3DTnandpKNhqbk2SZ9F2SkSrQkGslVa997fq33RydGGoLzBxUMqC'
       }
     )
-    # binding.pry
-    return html['extended_entities']['media'][0]['video_info']['variants'][-1]['url']
+    data = {
+      media_url: html['extended_entities']['media'][0]['media_url_https'],
+      video_url: html['extended_entities']['media'][0]['video_info']['variants'][-1]['url']
+    }
+    return data
   end
 end
